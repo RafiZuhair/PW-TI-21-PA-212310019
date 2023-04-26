7b15c0508fe4ecd4fa754b2e869bffcf7b1f13eb
RafiZuhair <113251032+RafiZuhair@users.noreply.github.com>
2023-04-26 08:22:55 +0700

    tugas praktikum 6-1

---
+++ CRUD/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\UsersController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [HomeController::class,'index']);
+Route::get('/home', [HomeController::class,'index']);
+
+Route::controller(ProductsController::class)->group(function () {
+    Route::get('/master-data/products', 'index')->name('m_products');
+    Route::post('/master-data/products', 'store');
+    Route::get('/master-data/products/{id}', 'show')->name('m_products_detail');
+    Route::get('/master-data/products/edit/{id}', 'edit')->name('m_products_edit');
+    Route::get('/master-data/products/remove/{id}', 'destroy')->name('m_products_remove');
+
+});
+
+Route::controller(UsersController::class)->group(function (){
+    Route::get('/tugas-data/users', 'index')->name('m_user');
+    Route::post('/tugas-data/users', 'store');
+    Route::get('/tugas-data/users/edit/{id}', 'edit')->name('m_user_edit');
+    Route::get('/tugas-data/users/remove/{id}', 'destroy')->name('m_user_remove');
+});
+
